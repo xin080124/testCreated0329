819e9cf0a696547107dabde2d59132317d0b0731
yx <hxy820718@163.com>
2017-03-29 20:22:06 +1300

    gallery and js test

--- action.js
+++ action.js
@@ -1,3 +1,4 @@
+/*
 window.onload = initPage;
 
 function initPage()
@@ -9,13 +10,82 @@ function initPage()
      alert("this is initPage");
      document.getElementById("confirm").onclick = function(evt){alert("the confirm button pressed!");};
      document.getElementById("damount").onblur = blurBamt;
+
 }
 
 function blurBamt()
 {
+   
    document.getElementById("damount").className = "ErrFld FontWhite FontYellow";
    alert("THIS is blur bamt !!!".toLowerCase());
    document.getElementById("damount").value = document.getElementById("damount").className;
    var a = 234;
    alert(parseInt(a,10).toString(16).toUpperCase()); //将A转换为10进制,然后再转换成16进制 同样也可以是其它进制 
+   
 }
+*/
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+//var slideIndex = 0;
+//showSlides();
+
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+//manual mode
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+
+
+//auto slideshow
+/*
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+*/
+
+function setNoVisibility(){  
+   document.getElementById("bjkgan").style.visibility = "hidden";  
+   document.getElementById("jqxsbma").style.visibility = "hidden";  
+   document.getElementById("sbjqxsbxx").style.visibility = "hidden";  
+   document.getElementById("jqxsbxx").style.visibility = "hidden";  
+   document.getElementById("sbjqxsbxxma").style.visibility = "hidden";   
+}  
+  
+function setVisibility(){  
+   document.getElementById("bjkgan").style.visibility = "visible";  
+   document.getElementById("jqxsbma").style.visibility = "visible";  
+   document.getElementById("sbjqxsbxx").style.visibility = "visible";  
+   document.getElementById("jqxsbxx").style.visibility = "visible";  
+   document.getElementById("sbjqxsbxxma").style.visibility = "visible";      
+}  
